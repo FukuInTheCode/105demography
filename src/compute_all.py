23908b067954122109a2182ea5128063f7982078
Tom C. <tom.carlier@epitech.eu>
2024-01-28 14:57:00 +0100

    [~] Error fix -> i forgot a :

--- src/compute_all.py
+++ src/compute_all.py
@@ -11,9 +11,9 @@ def compute_all(abrs: list[str]) -> int:
     total[0].sort()
     print("Country:")
     for country in total[0]:
-        print(f" {country}", end='')
-        if (country != total[0][-1])
-            print(",", end='')
+        print(f" {country}", end="")
+        if country != total[0][-1]:
+            print(",", end="")
     # a1, b1 = calc_fit1(total[1])
     # a2, b2 = calc_fit2(total[1])
     # calc_correlation(a1, b1, a2, b2)
