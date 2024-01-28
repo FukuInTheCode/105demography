b463600edf7322c078932f4fd243ec169115c6ae
Tom C. <tom.carlier@epitech.eu>
2024-01-28 14:57:25 +0100

    [~] update -> change gitignore

--- src/compute_all.py
+++ src/compute_all.py
@@ -1,4 +1,5 @@
 from src.parsing import get_data
+from src.calc_fit1 import calc_fit1
 
 
 def compute_all(abrs: list[str]) -> int:
@@ -7,15 +8,14 @@ def compute_all(abrs: list[str]) -> int:
 
     for i in range(57):
         for country in data:
-            total[1][i] += int(country[1 + i])
-        total[1][i] /= 1e6
+            total[1][i] += int(country[2 + i])
     total[0].sort()
-    print("Country:", end="")
+    print("Country:")
     for country in total[0]:
-        print(f" {country}", end="")
+        print(f" {country}", end='')
         if country != total[0][-1]:
-            print(",", end="")
-    # a1, b1 = calc_fit1(total[1:])
+            print(",", end='')
+    a1, b1 = calc_fit1(total[1:])
     # a2, b2 = calc_fit2(total[1:])
     # calc_correlation(a1, b1, a2, b2)
     return 0
