4776f798fcf9ef7ce2a258dbb0564ef71372fb20
Tom C. <tom.carlier@epitech.eu>
2024-01-28 14:57:33 +0100

    Merge branch 'main' into total_pop

--- src/compute_all.py
+++ src/compute_all.py
@@ -1,4 +1,5 @@
 from src.parsing import get_data
+from src.calc_fit_1 import calc_fit1
 from src.calc_fit_2 import calc_fit2
 
 
@@ -9,14 +10,15 @@ def compute_all(abrs: list[str]) -> int:
     for i in range(58):
         for country in data:
             total[1][i] += int(country[2 + i])
-        total[1][i] /= 1000000
+        total[1][i] /= 1e6
     total[0].sort()
     print("Country:", end="")
     for country in total[0]:
         print(f" {country}", end='')
         if country != total[0][-1]:
-            print(",", end='')
-    # a1, b1 = calc_fit1(total[1:])
+            print(",", end="")
+    print()
+    a1, b1 = calc_fit1(total[1:][::-1])
     a2, b2 = calc_fit2(total[1:])
     # calc_correlation(a1, b1, a2, b2)
     return 0
