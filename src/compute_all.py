595b6ca5d4e581cdf8cd750dcd54267ab2534a4d
Tom C. <tom.carlier@epitech.eu>
2024-01-28 14:57:23 +0100

    merge : [~] Update -> divide by 1e6 the pop for each year and remove the n after Country
    [~] Update -> divide by 1e6 the pop for each year and remove the n af…

--- src/compute_all.py
+++ src/compute_all.py
@@ -1,5 +1,4 @@
 from src.parsing import get_data
-from src.calc_fit1 import calc_fit1
 
 
 def compute_all(abrs: list[str]) -> int:
@@ -8,14 +7,15 @@ def compute_all(abrs: list[str]) -> int:
 
     for i in range(57):
         for country in data:
-            total[1][i] += int(country[2 + i])
+            total[1][i] += int(country[1 + i])
+        total[1][i] /= 1e6
     total[0].sort()
-    print("Country:")
+    print("Country:", end="")
     for country in total[0]:
-        print(f" {country}", end='')
+        print(f" {country}", end="")
         if country != total[0][-1]:
-            print(",", end='')
-    a1, b1 = calc_fit1(total[1:])
+            print(",", end="")
+    # a1, b1 = calc_fit1(total[1:])
     # a2, b2 = calc_fit2(total[1:])
     # calc_correlation(a1, b1, a2, b2)
     return 0
