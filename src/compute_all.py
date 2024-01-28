25d2ac173e95fdf87ab2df926d42abeaeb4c2d1f
Tom C. <tom.carlier@epitech.eu>
2024-01-28 14:57:28 +0100

    [~] make the calc_fit 1 :)

--- src/compute_all.py
+++ src/compute_all.py
@@ -1,22 +1,23 @@
 from src.parsing import get_data
-from src.calc_fit1 import calc_fit1
+from src.calc_fit_1 import calc_fit1
 
 
 def compute_all(abrs: list[str]) -> int:
     data = get_data(abrs)
-    total = [[country[0] for country in data], [0 for _ in range(57)], [i for i in range(1960, 2018)]]
+    total = [[country[0] for country in data], [0 for _ in range(58)], [i for i in range(1960, 2018)]]
 
-    for i in range(57):
+    for i in range(58):
         for country in data:
             total[1][i] += int(country[2 + i])
         total[1][i] /= 1e6
     total[0].sort()
     print("Country:", end="")
     for country in total[0]:
-        print(f" {country}", end='')
+        print(f" {country}", end="")
         if country != total[0][-1]:
-            print(",", end='')
-    a1, b1 = calc_fit1(total[1:])
+            print(",", end="")
+    print()
+    a1, b1 = calc_fit1(total[1:][::-1])
     # a2, b2 = calc_fit2(total[1:])
     # calc_correlation(a1, b1, a2, b2)
     return 0
