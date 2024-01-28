2ba1f702bff62ec53faf2f6b02858fe0c0712782
Tom C. <tom.carlier@epitech.eu>
2024-01-28 14:57:11 +0100

    [~] update -> add function for root mean square

--- src/calc_fit1.py
+++ src/calc_fit1.py
@@ -1,7 +1,8 @@
 import sys
+import math
 
 
-def calc_a1(data) -> int:
+def calc_a1(data) -> float:
     # n = len(data) ???
     # a lot of opti possible inside
     n = 57
@@ -14,18 +15,28 @@ def calc_a1(data) -> int:
     return a1
 
 
-def calc_a2(data) -> int:
+def calc_b1(data) -> float:
     n = 57
     sumX = sum(data[i][0] for i in range(n))
     sumY = sum(data[i][1] for i in range(n))
     sumXY = sum(data[i][0] * data[i][1] for i in range(n))
     sumXSquared = sum(data[i][0] ** 2 for i in range(n))
-    
+
     b1 = (sumY * sumXSquared - sumX * sumXY) / (n * sumXSquared - sumX ** 2)
     return b1
+
+
+def root_mean_square(data, a1, b1) -> int:
+    n = 57
+    values = [data[i][1] for i in range(n)]
+    predic = [a1 * data[i][0] + b1 for i in range(n)]
+    rms = math.sqrt(sum((values[i] - predic[i]) ** 2 for i in range(n)) / n)
+
+    return rms
 
 
 def calc_fit1(data) -> int:
     a1 = calc_a1(data)
     b1 = calc_b1(data)
+    rms = root_mean_square(data, a1, b1)
     return a1, b1
