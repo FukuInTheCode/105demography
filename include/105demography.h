c3a1d759b1c6ab80e0395dc25bb95761b83acd30
Tom C. <tom.carlier@epitech.eu>
2024-01-28 14:56:27 +0100

    [+] add -> init project

---
+++ include/105demography.h
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2024
+** 105demography .h
+** File description:
+** header for 105demography
+*/
+
+#pragma once
+    #define 105DEMOGRAPHY_H
+    #include <math.h>
+    #include <stdio.h>
+    #include <stdlib.h>
