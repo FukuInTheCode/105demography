e79c0639e292ce44126fbd024a02066cf5e931cf
Tom C. <tom.carlier@epitech.eu>
2024-01-28 14:57:16 +0100

    [~] Update -> Pass the list of the years to function

--- src/compute_all.py
+++ src/compute_all.py
@@ -3,7 +3,7 @@ from src.parsing import get_data
 
 def compute_all(abrs: list[str]) -> int:
     data = get_data(abrs)
-    total = [[country[0] for country in data], [0 for _ in range(57)]]
+    total = [[country[0] for country in data], [0 for _ in range(57)], [i for i in range(1960, 2018)]]
 
     for i in range(57):
         for country in data:
@@ -11,10 +11,10 @@ def compute_all(abrs: list[str]) -> int:
     total[0].sort()
     print("Country:")
     for country in total[0]:
-        print(f" {country}", end='')
+        print(f" {country}", end="")
         if country != total[0][-1]:
-            print(",", end='')
-    # a1, b1 = calc_fit1(total[1])
-    # a2, b2 = calc_fit2(total[1])
+            print(",", end="")
+    # a1, b1 = calc_fit1(total[1:])
+    # a2, b2 = calc_fit2(total[1:])
     # calc_correlation(a1, b1, a2, b2)
     return 0
