53bb6c1bd39947dcf8e7cf346ae5640f29065470
Tom C. <tom.carlier@epitech.eu>
2024-01-28 14:56:41 +0100

    [+] add -> add the parsing for the .csv

--- src/display_usage.py
+++
@@ -1,6 +0,0 @@
-def display_usage() -> int:
-    print("USAGE\n")
-    print("\t./105demography [code]+\n")
-    print("DESCRIPTION")
-    print("\tcode\tcountry code")
-    return 0
---
+++ src/parsing.py
@@ -0,0 +1,17 @@
+import csv
+import sys
+
+
+def get_data(abrs: list[str]) -> list[list[str]]:
+    listData = []
+
+    with open('../assets/105demography_data.csv', 'r') as csvfile:
+        content = csv.reader(csvfile, delimiter=';')
+        header = next(content)
+        abrs_index = header.index('Country Code')
+        for row in content:
+            if row[abrs_index] in abrs:
+                listData.append(row)
+        if len(listData) != len(abrs):
+            sys.exit(84)
+    return listData
